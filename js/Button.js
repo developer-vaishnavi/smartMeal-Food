2ce615ee93d3421e010a75cad58a30ed3a53a81d
Vaishnavi Sinha <mahisinha2113@gmail.com>
2024-08-11 22:48:15 +0530

    first commit

---
+++ js/Button.js
@@ -0,0 +1,5 @@
+const btnFormSubmit = document.querySelector(".btn--form");
+
+btnFormSubmit.addEventListener("click", function () {
+  window.alert("Awesome! Your form has been submitted successfully.");
+});
